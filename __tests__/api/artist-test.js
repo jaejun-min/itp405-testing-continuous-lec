7f2877d1e74fdbf5a9cf3bbc9562472f7da12e91
jaejunmin <jaejunmi@usc.edu>
2019-04-08 20:30:58 -0700

    adD

--- __tests__/api/artist-test.js
+++ __tests__/api/artist-test.js
@@ -3,11 +3,11 @@ const{Joi} = frisby;
 // const Joi = frisby.Joi;
 //
 it('should create an artist', ()=>{
-  // return frisby
-  //   .post('http://localhost:8000/api/artists', {
-  //     name: 'ITP'
-  //   })
-  //   .expect('status', 200)
-  //   .expect('json', 'name', 'ITP')
-  //   .expect('jsonTypes', 'id', Joi.number().required());
+  return frisby
+    .post('http://localhost:8000/api/artists', {
+      name: 'ITP'
+    })
+    .expect('status', 200)
+    .expect('json', 'name', 'ITP')
+    .expect('jsonTypes', 'id', Joi.number().required());
 });
--- __tests__/api/playlist-test.js
+++ __tests__/api/playlist-test.js
@@ -8,8 +8,8 @@ it('should return a 404 when deleting a playlist that exists', ()=>{
     .expect('status', 404);
 });
 
-// it('should return a 204 when deleting a playlist that does not exist', ()=>{
-//   return frisby
-//     .del('http://localhost:8000/api/playlists/6')
-//     .expect('status', 204);
-// });
+it('should return a 204 when deleting a playlist that does not exist', ()=>{
+  return frisby
+    .del('http://localhost:8000/api/playlists/6')
+    .expect('status', 204);
+});
